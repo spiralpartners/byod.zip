b56d55a2bbf78f5cf9940e4656d11dbb0c96a414
Hiroshi Igaki <github@musictale.net>
2017-11-23 07:00:13 +0900

    初期設定及びサンプルファイル投下 java01は課題ごとにフォルダ作成，java02はフォルダ作成なしでサンプルを置いている．デバッガが利用できる状態をとりあえず保持した．

---
+++ src/java02/Ex2-5.java
@@ -0,0 +1,20 @@
+class Ex2-5 {
+    public static void main(String[] args) {
+        System.out.println("2Hello world");
+        System.out.println("Hello world2");
+        System.out.println("Hello world3");
+        System.out.println("Hello world4");
+        System.out.println("Hello world5");
+        System.out.println("Hello world");
+        System.out.println("Hello world6");
+        System.out.println("Hello world7");
+        System.out.println("ほげ8");
+        System.out.println("おらおら");
+        System.out.println("おらおらおらおら");
+        System.out.println("おらおらおら");
+        Test3 t = new Test3();
+        t.fuga = 3;
+        System.out.println(t.getFuga()+1);
+        System.out.println(t.getHoge());        
+    }
+}
