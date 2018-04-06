2cbccff6d7af2a1ed284e8f69843e15e187cdf7b
Hiroshi Igaki <github@musictale.net>
2018-04-06 15:39:34 +0900

    update

---
+++ java_kadai/java01/Hello.java
@@ -0,0 +1,16 @@
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world2");
+        System.out.println("Hello world3");
+        System.out.println("Hello world4");
+        System.out.println("ほげ8");
+        System.out.println("おらおら");
+        System.out.println("おらおらおらおら");
+        System.out.println("おらおらおら");
+        Test2 t = new Test2();
+        t.fuga = 3;
+        System.out.println(t.getFuga()+1);
+        System.out.println(t.getHoge());
+    }
+}
