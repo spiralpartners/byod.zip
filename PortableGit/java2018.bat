f99b7a94689a7e1a8cb6426c09196b0724665d99
Hiroshi Igaki <github@musictale.net>
2018-10-13 17:40:57 +0900

    cd処理を追加

--- PortableGit/java2018.bat
+++ PortableGit/java2018.bat
@@ -1,3 +1,3 @@
+cd %USERPROFILE%\oithomes\java2018
 @set MSYSTEM=MINGW64
-@set HOME=
-@C:\oit\PortableGit-2.17.0-64\usr\bin\mintty.exe C:\oit\PortableGit-2.17.0-64\usr\bin\bash.exe --login -i
+@C:\oit\PortableGit-2.19.1-64\usr\bin\mintty.exe C:\oit\PortableGit-2.19.1-64\usr\bin\bash.exe --login -i
