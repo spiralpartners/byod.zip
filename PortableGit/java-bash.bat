d95a904003d9a60c9ef72d42b7e71286f86cb49c
Hiroshi Igaki <github@musictale.net>
2018-11-20 15:44:57 +0900

    bat vbsファイル削除

--- PortableGit/java-bash.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-IF EXIST "%USERPROFILE%\oithomes\java\" (
-cd %USERPROFILE%\oithomes\java
-)
-@set MSYSTEM=MINGW64
-@C:\oit\PortableGit-2.19.1-64\usr\bin\mintty.exe C:\oit\PortableGit-2.19.1-64\usr\bin\bash.exe --login -i
--- PortableGit/java2018.vbs
+++
@@ -1,2 +0,0 @@
-Set WshShell = WScript.CreateObject("WScript.Shell")
-Return = WshShell.Run("C:\oit\PortableGit-2.19.1-64\java-bash.bat", 0, true)
